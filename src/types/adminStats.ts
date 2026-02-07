679204018224fb2cdc449e838e58e244ffad5f90
Kovacevic22 <vukasinkue03@gmail.com>
2026-02-08 00:48:59 +0100

    Stats endpoint implemented

---
+++ src/types/adminStats.ts
@@ -0,0 +1,14 @@
+export interface AdminStats {
+    totalUsers: number;
+    totalCompanies: number;
+    totalStudents: number;
+    bannedUsers: number;
+    pendingCompanies: number;
+    totalAds: number;
+    activeAds: number;
+    expiredAds: number;
+    totalApplications: number;
+    pendingApplications: number;
+    acceptedApplications: number;
+    rejectedApplications: number;
+}
